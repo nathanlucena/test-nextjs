ebe6edbe372746db7239e5e5da1ada106bd47a05
nathan lucena <nathan.lucena89@gmail.com>
2021-06-01 15:21:22 -0300

    Start next-test

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+
+const aa=1 
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1>  oi </h1>
+      </main>
+    </div>
+  )
+}
